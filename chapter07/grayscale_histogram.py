8c4d0939660e0d3b82c51f7da2aa20c05100fe57
Gaurav Kumar <gaurav.kumar@jumbotail.com>
2018-08-26 16:54:00 +0530

    Adding beach image

---
+++ chapter07/grayscale_histogram.py
@@ -0,0 +1,13 @@
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Original", image)
+
+
+

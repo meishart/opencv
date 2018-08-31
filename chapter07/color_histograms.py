dc77aa860143822bec527e24b39a5df191ebac0e
Gaurav Kumar <gaurav.kumar@jumbotail.com>
2018-08-31 19:51:14 +0530

    Adding histograms

--- chapter07/color_histograms.py
+++ chapter07/color_histograms.py
@@ -7,17 +7,19 @@ ap.add_argument("-i", "--image", required=True, help="path to the image")
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Original", image)
-#cv2.waitKey(0)
-
-hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+channels = cv2.split(image)
+colors = ("b", "g", "r")
 
 plt.figure()
-plt.title("Grayscale Histogram")
+plt.title("Flattened Color Histogram")
 plt.xlabel("Bins")
 plt.ylabel("# of Pixels")
-plt.plot(hist)
-plt.xlim([0, 256])
+
+for (channel, color) in zip(channels, colors):
+    hist = cv2.calcHist([channel], [0], None, [256], [0, 256])
+    plt.plot(hist, color = color)
+    plt.xlim([0, 256])
+
 plt.show()
 cv2.waitKey(0)

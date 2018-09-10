c5982d6346710cc62e7d3b64dfb45943ecd75b3f
Gaurav Kumar <gaurav.kumar@jumbotail.com>
2018-09-10 17:29:43 +0530

    Cropping coins from the original image

--- chapter11/counting_coins.py
+++ chapter11/counting_coins.py
@@ -1,6 +1,7 @@
 import argparse
 import imutils
 import cv2
+import numpy as np
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to the image")
@@ -21,5 +22,18 @@ print("I count {} coins in this image.".format(len(counts)))
 coins = image.copy()
 cv2.drawContours(coins, counts, -1, (0, 255, 0), 2)
 cv2.imshow("Coins", coins)
+
+for (i, c) in enumerate(counts):
+    (x, y, w, h) = cv2.boundingRect(c)
+
+    print("Coins #{}".format(i + 1))
+    coin = image[y:y+h, x:x+w]
+    cv2.imshow("Coin", coin)
+
+    mask = np.zeros(image.shape[:2], dtype="uint8")
+    ((centerX, centerY), radius) = cv2.minEnclosingCircle(c)
+    cv2.circle(mask, (int(centerX), int(centerY)), int(radius), 255, -1)
+    mask = mask[y:y+h, x:x+w]
+    cv2.imshow("Masked Coin", cv2.bitwise_and(coin, coin, mask=mask))
 
 cv2.waitKey(0)
